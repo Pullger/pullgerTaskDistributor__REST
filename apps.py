c0f7567c680f4b502ff425d4d7cd7e451ce13811
NeoUKR <neokkv@gmail.com>
2022-10-24 16:59:49 +0300

    init

---
+++ apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PullgertaskdistributorRestConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pullgerTaskDistributor__REST'
